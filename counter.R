be8b66f52af69dec9ace633f5dccd16d1cfd0b3f
Veer Gujral <veergujral11@gmail.com>
2026-07-22 14:01:08 +1000

    Added derement button 2

--- counter.R
+++ counter.R
@@ -86,4 +86,49 @@ server <- function(input, output, session) {
 
 shinyApp(ui, server)
 
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Simple counter application"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      actionButton("inc", "Increment"),
+      actionButton("dec", "Decrement"),
+      actionButton("reset", "Reset"),
+    ),
+    
+    mainPanel(
+      h3("Current count:"),
+      textOutput("count")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  # Create reactiveVal to store the count
+  count <- reactiveVal(0)
+  
+  # Define an observer to update count based on button clicks
+  observeEvent(input$inc, {
+    count(count() + 1)
+  })
+  
+  observeEvent(input$dec, {
+    count(count() - 1)
+  })
+  
+  observeEvent(input$reset, {
+    count(0)
+  })
+  
+  # Render the current count
+  output$count <- renderText({
+    count()
+  })
+}
+
+shinyApp(ui, server)
+
+
 
